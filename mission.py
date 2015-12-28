3658c43103f844fbc54d441e80cb7ff493403bca
Maciej Stachowiak <maciej.s@me.com>
2015-12-28 15:52:21 -0800

    Fix bug in output formatting logic with BGE.

--- mission.py
+++ mission.py
@@ -60,7 +60,7 @@ def mission_params(funds, bge):
 
 def format_mission_results(member, mission, funds, bge, result):
     funded_string = 'unfunded' if funds == '0' else 'funded'
-    bge_string = bge if bge == '' else 'no BGE'
+    bge_string = bge if bge != '' else 'no BGE'
     result_string = 'MISSION: ' + mission + ' for ' + member + ' (' + funded_string + ', ' + bge_string + '): ' + result
 
     return result_string
